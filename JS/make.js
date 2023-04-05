42b91ac8d3192a7f8d24b2f2220a357b34f60301
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2023-04-05 12:58:38 -0300

    Add files via upload

---
+++ JS/make.js
@@ -0,0 +1,8 @@
+class Produtos{
+    constructor(nome, descricao, preco, quantidade){
+        this.nome = nome,
+        this.descricao = descricao,
+        this.preco = preco,
+        this.quantidade = quantidade
+    }
+}
